9acc4af414447f67a886a06865091efa5f2aa049
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-22 10:48:38 +0000

    Add files via upload
    adding a button

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,4 @@
-var mySong, amplitude;
+var mySong, amplitude, play;
 
 function preload () {
     mySong = loadSound('assets/massiveAttackAngel.mp3');
@@ -11,18 +11,22 @@ function setup() { //runs once
     background(0);
 
     amplitude = new p5.Amplitude();
-    background(255)
+    background(220)
+    rectMode(CENTER);
+    play = false;
+    var button = createButton("Play");
+    button.center();
+    button.addClass('btn');
 }
 
 //document object model
 //window object model - where you can find current width + height
+//FLASH WARNING
 
 function draw() {
     var level = amplitude.getLevel();
     var vol = map(level, 0, 1, 0, 1000);
     var col = map(level, 0, 1, 0, 255);
-    
-    background(col)
 
     fill(col, 255, 255);
     ellipse(width/2, height/2, vol);
