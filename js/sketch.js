bdeaaddc306e242c96e0c07ff880dda8bd08e7e5
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-22 10:30:06 +0000

    Add files via upload
    FLASH WARNING - changing the width and colour with the amplitude of the music

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,32 @@
-function setup() {
-    var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+var mySong, amplitude;
+
+function preload () {
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
 }
 
-function draw() {
-    background(220);
+function setup() { //runs once
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.parent("p5container");
+    mySong.play();
+    background(0);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    amplitude = new p5.Amplitude();
+    background(255)
+}
+
+//document object model
+//window object model - where you can find current width + height
+
+function draw() {
+    var level = amplitude.getLevel();
+    var vol = map(level, 0, 1, 0, 1000);
+    var col = map(level, 0, 1, 0, 255);
+    
+    background(col)
+
+    fill(col, 255, 255);
+    ellipse(width/2, height/2, vol);
+
+    fill(col, 255, 255);
+    ellipse(width/2, height/2, vol/2);
 }
